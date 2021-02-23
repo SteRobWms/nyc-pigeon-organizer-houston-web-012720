83749d500efcb31095ea89a68ce656bfa70c5d5b
SteRobWms <stephen.r.williams.87@gmail.com>
2021-02-22 21:01:14 -0600

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
+require 'pry'
+
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+    birdHash = {}
+    data.each do |trait, values|
+        values.each do |value, nameArr|
+            nameArr.each do |name|
+                if birdHash[name].nil?
+                    birdHash[name] = {}
+                end
+                if birdHash[name][trait].nil?
+                    birdHash[name][trait] = [value.to_s]
+                else
+                    birdHash[name][trait] << value.to_s
+                end
+            end
+        end
+    end
+    birdHash
 end
